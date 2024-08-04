bdad7b16e77d5f0f8353d53562c193dc3eda5c13
aditya-1310 <bhadadeaditya1310@gmail.com>
2024-08-04 21:28:28 +0530

    n

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log('Connected to MongoDB!');
+    console.log('Connected too MongoDB!');
   })
   .catch((err) => {
     console.log(err);
